e0b41ed8247e63bd2d0684555abc0b70ddd9170e
Hari Parameswaran <harisudhan231@gmail.com>
2024-11-24 10:59:20 -0800

    auto gen

---
+++ Bhaagi/BhaagiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BhaagiApp.swift
+//  Bhaagi
+//
+//  Created by Hari Parameswaran on 11/24/24.
+//
+
+import SwiftUI
+
+@main
+struct BhaagiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
